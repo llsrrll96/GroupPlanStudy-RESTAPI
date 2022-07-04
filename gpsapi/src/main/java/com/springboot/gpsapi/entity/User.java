f9dc7c663a76fbb77eb181daf1f0bc2d52a8da94
kim95 <dlsxj2188@gmail.com>
2022-07-04 14:44:56 +0900

    add User Column

--- gpsapi/src/main/java/com/springboot/gpsapi/entity/User.java
+++ gpsapi/src/main/java/com/springboot/gpsapi/entity/User.java
@@ -1,5 +1,23 @@
 package com.springboot.gpsapi.entity;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+@Entity
+public class User {
+	
+	@Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long uid;
+	@Column(unique = true)
+	private String email;
+	private String password;
+	private String introduce;
+	private String nickname;
 }
