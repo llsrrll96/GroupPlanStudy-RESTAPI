809282e2fdd9cc668b4a4b14ce3f87909ea33340
llsrrll96 <llsrrll96@gmail.com>
2022-07-04 11:34:32 +0900

    2022 07 04 init

---
+++ gpsapi/src/main/java/com/springboot/gpsapi/GpsapiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.gpsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsapiApplication.class, args);
+	}
+
+}
---
+++ gpsapi/src/test/java/com/springboot/gpsapi/GpsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.gpsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
