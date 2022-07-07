09680ab9db6ea2d409c94a191c31b10bc57904b2
llsrrll96 <llsrrll96@gmail.com>
2022-07-07 16:21:38 +0900

    add findUserInUids method

--- gpsapi/src/main/java/com/springboot/gpsapi/repository/LoginRepository.java
+++ gpsapi/src/main/java/com/springboot/gpsapi/repository/LoginRepository.java
@@ -1,9 +1,17 @@
 package com.springboot.gpsapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.springboot.gpsapi.entity.User;
 
 public interface LoginRepository  extends JpaRepository<User, Long>{
+
+	@Query(value = "select * from user u where u.uid in :uids",
+			nativeQuery = true) // 동적으로 쿼리 날릴때 true
+	List<User> findUserInUids(@Param("uids") Long[] uids);
 
 }
