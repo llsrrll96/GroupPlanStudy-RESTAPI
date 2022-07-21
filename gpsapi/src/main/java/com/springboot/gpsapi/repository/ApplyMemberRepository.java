c42c260ac3b96ef61b3d238b54f494d18d567661
llsrrll96 <llsrrll96@gmail.com>
2022-07-21 23:35:01 +0900

    update applymember

--- gpsapi/src/main/java/com/springboot/gpsapi/repository/ApplyMemberRepository.java
+++ gpsapi/src/main/java/com/springboot/gpsapi/repository/ApplyMemberRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.repository.query.Param;
 
 import com.springboot.gpsapi.entity.GroupApplyMember;
 import com.springboot.gpsapi.entity.GroupRoom;
+import com.springboot.gpsapi.entity.User;
 
 public interface ApplyMemberRepository extends JpaRepository<GroupApplyMember, Long>
 {
-
 	List<GroupApplyMember> findByGroupRoom(GroupRoom groupRoom);
 
-	void deleteGmIdByGroupRoomAndUid(GroupRoom groupRoom, Long uid);
+	void deleteGmIdByGroupRoomAndUser(GroupRoom groupRoom, User user);
 
 //	@Query(name = "SELECT gm_id, gr_id FROM group_apply_member WHERE gr_id = :grId", nativeQuery = true)
 //	List<GroupApplyMember> selectAllByGrId(@Param("gr_id") long grId);
